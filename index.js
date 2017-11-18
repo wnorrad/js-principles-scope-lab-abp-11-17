90c23f3f5ba5ac0c6c733c2de73bc06904bcdb6c
Whitney Norrad <wnorrad@gmail.com>
2017-11-18 03:30:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+  }
+  
+  function setBestCustomer(){
+   bestCustomer = 'not bob';
+  }
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+ 
+ const leastFavoriteCustomer = 'potato';
+ 
+ function changeLeastFavoriteCustomer() {
+   leastFavoriteCustomer = 'not potato';
+ }
+ 
+ function attemptTwoFavoriteCustomers(){
+ const favoriteCustomer = 'craig';
+ const favoriteCustomer = 'not craig';
+ }
